973a8ac2b70cdcb6637653bfd58db1a5a4601908
prakash-innovatics <66667634+prakash-innovatics@users.noreply.github.com>
2020-09-10 14:32:38 +0530

    Services Added

--- src/pages/services.js
+++ src/pages/services.js
@@ -107,41 +107,7 @@ const InnovaticServices = ({ data, location }) => {
                 }
             </div>
             
-            <div className="axil-call-to-action-area shape-position ax-section-gap theme-gradient">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="axil-call-to-action">
-                                <div className="section-title text-center">
-                                    <span className="sub-title extra04-color wow" data-splitting>Let's work together</span>
-                                    <h2 className="title wow" data-splitting>Need a successful project?</h2>
-                                    <a className="axil-button btn-large btn-transparent" href="#"><span
-                                className="button-text">Estimate Project</span><span className="button-icon"></span></a>
-                                    <div className="callto-action">
-                                        <span className="text wow" data-splitting>Or call us now</span>
-                                        <span className="wow" data-splitting><i className="fal fa-phone-alt"></i> <a href="#">(123)
-                                    456 7890</a></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="shape-group">
-                    <div className="shape shape-01">
-                        <i className="icon icon-shape-14"></i>
-                    </div>
-                    <div className="shape shape-02">
-                        <i className="icon icon-shape-09"></i>
-                    </div>
-                    <div className="shape shape-03">
-                        <i className="icon icon-shape-10"></i>
-                    </div>
-                    <div className="shape shape-04">
-                        <i className="icon icon-shape-11"></i>
-                    </div>
-                </div>
-            </div>
+            
         </main>
         <HomeCTA></HomeCTA>
       </Layout>
